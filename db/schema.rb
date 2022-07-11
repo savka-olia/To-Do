7bf968678a8147ceef7b4d52841b07b7f666e91c
savka-olia <tokarchykolia@gmail.com>
2022-07-11 19:38:29 +0300

    add boolean row to the Tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_10_210322) do
+ActiveRecord::Schema.define(version: 2022_07_11_163346) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
